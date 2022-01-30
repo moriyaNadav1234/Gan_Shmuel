9a40f17f4f346701d2525fa54cdbbe41e720f7a7
shZsektor <sektor_sharef@hotmail.com>
2022-01-30 14:48:12 +0200

    /health and /Provider api

---
+++ Billing/src/api.py
@@ -0,0 +1,29 @@
+from src import app
+from flask import request
+import json
+import mysql.connector
+
+
+mydb = mysql.connector.connect(
+    host="172.17.0.2",
+    user="root",
+    password="catty",
+    database="billdb"
+)
+
+mycursor = mydb.cursor()
+
+@app.route("/health")
+def index():
+    return "OK"
+
+@app.route('/provider',methods=['POST'])
+def Insert_provider():
+    sql = "INSERT INTO Provider (id,name) VALUES (%s,%s)"
+    #print(request.form['name'])
+    val = ('9',request.form['name'])
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    return "record inserted"
